9aff08898231442a1eb0435ca91e45455a988a0b
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-10-14 10:43:45 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+// API: https://emkc.org/api/v2/piston/
+
+function App() {
+  return (
+    <>
+      <div>
+        <div id="input"></div>
+        <div id="output"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
