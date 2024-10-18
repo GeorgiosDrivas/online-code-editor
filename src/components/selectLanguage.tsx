4f24e1b8ad110c7f254d8f8fc6ff30823fbf4ad5
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-10-18 12:07:47 +0300

    components refactor

---
+++ src/components/selectLanguage.tsx
@@ -0,0 +1,23 @@
+import { languages } from "../constants";
+import { Option } from "../type";
+
+export default function SelectLanguage({ lng, setLanguage }: any) {
+  return (
+    <select
+      id="selected-language"
+      value={lng?.language}
+      onChange={(e) =>
+        setLanguage(
+          languages.find((item: Option) => item.language === e.target.value) ||
+            languages[0]
+        )
+      }
+    >
+      {languages.map((option) => (
+        <option value={option.language} key={option.id}>
+          {option.language}
+        </option>
+      ))}
+    </select>
+  );
+}
